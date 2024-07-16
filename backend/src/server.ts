ed5a9d7907ecb39aca0aa66159c0b3b00eaff8f2
David Requeno <davidrequeno52@gmail.com>
2024-07-15 20:44:41 -0600

    ...

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,6 @@
-import express, { text } from "express";
+import express from "express";
 import multer from "multer"; // Middleware para manejar datos multipart/form-data (usado para subir archivos)
 import cors from 'cors'; // Middleware para habilitar CORS (Cross-Origin Resource Sharing)
-import path from 'path'; // Módulo para manejar rutas de archivos
 import fs from 'fs'; // Módulo para interactuar con el sistema de archivos
 import { PDFExtract } from "pdf.js-extract"; // Librería para extraer texto de archivos PDF
 
